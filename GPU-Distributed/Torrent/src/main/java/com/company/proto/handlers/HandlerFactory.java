5d8ae6b8751cc2cfa58a413192cfe46bf9443b76
Tudor Pop <tudor14pop@gmail.com>
2018-01-16 20:42:38 +0200

    Added all types of messages in factory

--- GPU-Distributed/Torrent/src/main/java/com/company/proto/handlers/HandlerFactory.java
+++ GPU-Distributed/Torrent/src/main/java/com/company/proto/handlers/HandlerFactory.java
@@ -8,11 +8,25 @@ import java.util.Map;
 
 public class HandlerFactory {
 	private static Map<Object, ByteString> storage = new HashMap<>();
+	
 	public static Handler create(Torrent.Message message) {
-		if (message.hasLocalSearchRequest())
-			return new LocalSearchHandler();
-		else if (message.hasUploadRequest())
+		if (message.hasUploadRequest())
 			return new UploadRequest(storage);
+		else if (message.hasReplicateRequest())
+			return new ReplicateHandler();
+		else if (message.hasLocalSearchRequest())
+			throw new RuntimeException("Invalid Message Type");
+		else if (message.hasLocalSearchRequest())
+			return new LocalSearchHandler();
+		else if (message.hasSearchRequest())
+			throw new RuntimeException("Invalid Message Type");
+		else if (message.hasSearchRequest())
+			throw new RuntimeException("Invalid Message Type");
+		else if (message.hasDownloadRequest())
+			throw new RuntimeException("Invalid Message Type");
+		else if (message.hasChunkRequest())
+			throw new RuntimeException("Invalid Message Type");
+		
 		throw new RuntimeException("Invalid Message Type");
 	}
 }
---
+++ GPU-Distributed/Torrent/src/main/java/com/company/proto/handlers/ReplicateHandler.java
@@ -0,0 +1,10 @@
+package com.company.proto.handlers;
+
+import com.company.proto.torrent.Torrent;
+
+public class ReplicateHandler implements Handler {
+	@Override
+	public Torrent.Message handle(Torrent.Message message) {
+		return null;
+	}
+}
