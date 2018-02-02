f128e84ff4d98a20f13300585cfc98af7ad32ffc
Tudor Pop <tudor14pop@gmail.com>
2018-02-02 13:02:27 +0200

    Upload request revised

--- GPU-Distributed/Torrent/src/main/java/com/company/proto/handlers/UploadRequest.kt
+++ GPU-Distributed/Torrent/src/main/java/com/company/proto/handlers/UploadRequest.kt
@@ -8,7 +8,9 @@ import com.company.proto.torrent.Torrent
 import com.google.protobuf.ByteString
 
 
-class UploadRequest(private var storage: MutableMap<Torrent.FileInfo, ByteString>) : Handler {
+class UploadRequest(
+		private var storage: MutableMap<Torrent.FileInfo, ByteString>
+) : Handler {
 	
 	override fun handle(message: Torrent.Message): Torrent.Message {
 		val filename = message.uploadRequest.filename
@@ -22,19 +24,10 @@ class UploadRequest(private var storage: MutableMap<Torrent.FileInfo, ByteString
 				.setFilename(filename)
 				.addAllChunks(data.toChunkedArray(CHUNK_SIZE))
 				.build()
+		if (fileInfo in storage.keys) return successFileUploadResponse(fileInfo)
+		
 		storage[fileInfo] = data
 		return successFileUploadResponse(fileInfo)
-	}
-	
-	private fun errorFilenameEmptyResponse(): Torrent.Message {
-		val uploadResponse = Torrent.UploadResponse.newBuilder()
-				.setStatus(Torrent.Status.MESSAGE_ERROR)
-				.setErrorMessage("Filename is empty")
-				.build()
-		return Torrent.Message.newBuilder()
-				.setType(Torrent.Message.Type.UPLOAD_RESPONSE)
-				.setUploadResponse(uploadResponse)
-				.build()
 	}
 	
 	private fun successFileUploadResponse(fileInfo: Torrent.FileInfo): Torrent.Message {
@@ -42,6 +35,17 @@ class UploadRequest(private var storage: MutableMap<Torrent.FileInfo, ByteString
 		val uploadResponse = Torrent.UploadResponse.newBuilder()
 				.setStatus(Torrent.Status.SUCCESS)
 				.setFileInfo(fileInfo)
+				.build()
+		return Torrent.Message.newBuilder()
+				.setType(Torrent.Message.Type.UPLOAD_RESPONSE)
+				.setUploadResponse(uploadResponse)
+				.build()
+	}
+	
+	private fun errorFilenameEmptyResponse(): Torrent.Message {
+		val uploadResponse = Torrent.UploadResponse.newBuilder()
+				.setStatus(Torrent.Status.MESSAGE_ERROR)
+				.setErrorMessage("Filename is empty")
 				.build()
 		return Torrent.Message.newBuilder()
 				.setType(Torrent.Message.Type.UPLOAD_RESPONSE)
